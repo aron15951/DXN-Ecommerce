8b7c25161aae8b9a735acb3eab1c5cf2e8cc6047
aron15951 <aronchoque72@gmail.com>
2024-11-13 15:46:28 -0500

    feat: Add initial NavBar component

--- Front/src/components/NavBar.jsx
+++ Front/src/components/NavBar.jsx
@@ -1,3 +1,120 @@
+import styled from "styled-components";
+import { FiShoppingCart, FiSearch } from "react-icons/fi";
+import { FaRegCircleUser } from "react-icons/fa6";
+
+// Componente de barra de navegación
 export default function NavBar() {
-    return <h1>NavBar</h1>;
+  return (
+    <Container>
+      <ContainerIcon>
+        <h1 className="text__icon">SHOP.CO</h1>
+      </ContainerIcon>
+      <NavList>
+        <li>
+          <NavItem href="">Shop</NavItem>
+        </li>
+        <li>
+          <NavItem href="">On Sale</NavItem>
+        </li>
+        <li>
+          <NavItem href="">New Arrivals</NavItem>
+        </li>
+        <li>
+          <NavItem href="">Brands</NavItem>
+        </li>
+      </NavList>
+      <SearchForm>
+        <SearchIcon>
+          <FiSearch />
+        </SearchIcon>
+        <input type="text" placeholder="Search" />
+      </SearchForm>
+      <Icons>
+        <FiShoppingCart size={20} />
+        <FaRegCircleUser size={20} />
+      </Icons>
+    </Container>
+  );
 }
+
+// Estilos de los componentes
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  height: 5rem;
+`;
+
+const ContainerIcon = styled.div`
+  height: 3rem;
+  display: flex;
+  align-items: center;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+  height: 3rem;
+  margin: 0;
+  padding: 0;
+
+  li {
+    list-style: none;
+  }
+`;
+
+const NavItem = styled.a`
+  margin: 0 1rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
+  font-family: "satoshi", sans-serif;
+  font-size: 16px;
+
+  &:hover {
+    color: #666;
+  }
+`;
+
+const SearchForm = styled.form`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  input {
+    padding: 0.5rem 2rem; 
+    border: 1px solid #ccc;
+    border-radius: 1rem;
+    margin-right: 0.5rem;
+    font-family: "satoshi", sans-serif;
+    font-size: 16px;
+    outline: none;
+
+    &:focus {
+      border-color: #666;
+    }
+  }
+`;
+
+const SearchIcon = styled.div`
+  position: absolute;
+  left: 0.5rem;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #666;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  cursor: pointer;
+
+  svg {
+    color: #333;
+    &:hover {
+      color: #666;
+    }
+  }
+`;
--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GlobalStyle } from "../src/styles/GlobalStyles";
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouterProvider router={router} />
   </React.StrictMode>
 )
+
